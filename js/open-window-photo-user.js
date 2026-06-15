68893912035366909b7fca764fc0f734cdb0aabd
Никита Межецкий <nsm020598@gmail.com>
2026-06-15 14:38:17 +1000

    Добавление комментариев с кнопки

--- js/open-window-photo-user.js
+++ js/open-window-photo-user.js
@@ -1,71 +1,73 @@
 const CLASS_HIDDEN = 'hidden';
-const CLASS_MODAL_OPEN = 'modal-open'
+const CLASS_MODAL_OPEN = 'modal-open';
+const STEP_COUNT_COMMENTS = 5;
 const windowPhotoUser = document.querySelector('section.big-picture');
+const containerComments = windowPhotoUser.querySelector('ul.social__comments');
 const elementCommentLi = windowPhotoUser.querySelector('ul.social__comments li');
+const [countCommentsElement, maxCountCommentsElement] = windowPhotoUser.querySelector('.social__comment-count').childNodes;
+const showMoreButton = windowPhotoUser.querySelector('button.social__comments-loader');
 const bodyElement = document.querySelector('body')
+
+const infoCommentsView = {
+  comments: [],
+  countComments: 0
+};
+
+const drawComments = () => {
+  console.log(infoCommentsView.countComments);
+  infoCommentsView.comments.slice(infoCommentsView.countComments, infoCommentsView.countComments + STEP_COUNT_COMMENTS).forEach((comment) => {
+    const cloneCommentElement = elementCommentLi.cloneNode(true);
+    cloneCommentElement.querySelector('img.social__picture').src = comment.avatar;
+    cloneCommentElement.querySelector('p.social__text').textContent = comment.message;
+    containerComments.append(cloneCommentElement);
+  })
+  infoCommentsView.countComments += STEP_COUNT_COMMENTS;
+  countCommentsElement.textContent = countCommentsElement.textContent.replace(/\d+/, Math.min(infoCommentsView.comments.length, infoCommentsView.countComments));
+  console.log(infoCommentsView.countComments);
+  if (infoCommentsView.comments.length <= infoCommentsView.countComments) {
+    showMoreButton.classList.add(CLASS_HIDDEN);
+  }
+};
+
+const showMoreButtonClickHandler = (evt) => {
+  evt.preventDefault();
+  drawComments()
+}
 
 const closeModalWindow = (evt) => {
   evt.preventDefault();
+  infoCommentsView.comments = [];
+  infoCommentsView.countComments = 0;
   windowPhotoUser.classList.add(CLASS_HIDDEN);
   bodyElement.classList.remove(CLASS_MODAL_OPEN)
-  document.removeEventListener('keydown', closeModalWimdowKeydownEsc)
+  document.removeEventListener('keydown', closeModalWindowKeydownEsc)
 };
 
-const closeModalWimdowKeydownEsc = (evt) => {
-  if(evt.key === 'Escape'){
+const closeModalWindowKeydownEsc = (evt) => {
+  if (evt.key === 'Escape') {
     closeModalWindow(evt);
   }
 }
 
-const drawCommentUser = ({comments}, container) => {
-  const listComments = container.querySelectorAll('li');
-
-  const drawDataComments = (elementComment, {avatar, name, message}) => {
-    const imgComment = elementComment.querySelector('img');
-    imgComment.src = avatar;
-    imgComment.alt = name;
-    elementComment.querySelector('p').textContent = message;
-  }
-
-  if (listComments.length > comments.length) {
-    listComments.forEach((item, index) => {
-      if (index + 1 > comments.length) {
-        item.remove();
-      }
-    })
-  }
-
-  comments.forEach((comment, index) => {
-    if (listComments.length - 1 >= index) {
-      drawDataComments(listComments[index], comment)
-      return
-    }
-
-    const commentElement = elementCommentLi.cloneNode(true);
-    drawDataComments(commentElement, comment);
-    container.append(commentElement);
-  })
-}
-
-
 const openWindowPhotoUser = (photo) => {
-  const containerComments = windowPhotoUser.querySelector('ul.social__comments');
   const buttonClose = windowPhotoUser.querySelector('button[type="reset"]')
+  containerComments.textContent = '';
+
+  infoCommentsView.comments = photo.comments;
+  showMoreButton.classList.remove(CLASS_HIDDEN);
+  maxCountCommentsElement.textContent = photo.comments.length;
+  drawComments();
 
   bodyElement.classList.add(CLASS_MODAL_OPEN);
   windowPhotoUser.classList.remove(CLASS_HIDDEN);
-  windowPhotoUser.querySelector('button.social__comments-loader').classList.add(CLASS_HIDDEN);
-  windowPhotoUser.querySelector('div.social__comment-count').classList.add(CLASS_HIDDEN);
-
   windowPhotoUser.querySelector('div.big-picture__img img').src = photo.url;
   windowPhotoUser.querySelector('div.big-picture__social p.social__likes span')
     .textContent = photo.likes;
   windowPhotoUser.querySelector('div.big-picture__social p.social__caption').textContent = photo.description;
 
-  drawCommentUser(photo, containerComments)
-
   buttonClose.addEventListener('click', closeModalWindow)
-  document.addEventListener('keydown', closeModalWimdowKeydownEsc)
+  document.addEventListener('keydown', closeModalWindowKeydownEsc)
 }
 
-export {openWindowPhotoUser}
+showMoreButton.addEventListener('click', showMoreButtonClickHandler)
+export { openWindowPhotoUser }
